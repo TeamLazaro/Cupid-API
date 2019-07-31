516b4bf0cf77de92df8a70b227689d4bf782abcf
adi <52442230+adi-lazaro@users.noreply.github.com>
2019-07-31 10:02:51 +0530

    Services : Research People : Add this service

--- post-deploy.sh
+++ post-deploy.sh
@@ -23,3 +23,23 @@ npm install
 # Reload the node processes
 # -/-/-/-/-
 pm2 reload "cupid"
+
+# -/-/-/-/-
+# Set up all the scheduled tasks
+# -/-/-/-/-
+if [ -f setup/tasks.crontab ]; then
+	# Set permissive permissions for the scheduled task scripts
+	chmod 744 */scheduled-tasks/*
+	# Build a cumulative, consolidated crontab
+	TASKS_DIR="`pwd`/services"
+	LOG_DIR="`pwd`/environment/logs"
+	CRON_ENV="\n\nPATH=/bin:/usr/bin:/usr/local/bin:${TASKS_DIR}\nHOME=${LOG_DIR}\n";
+	find -type f -name '*.crontab' -exec cat {} \; > tmp_crontab;
+	printf $CRON_ENV | cat - tmp_crontab | tee tmp_2_crontab;
+	rm tmp_crontab;
+
+	# Copy the consolidated scheduled tasks file to the environment
+	mv tmp_2_crontab environment/scheduled-tasks/$PROJECT_DIR.crontab
+	# Re-establish all the scheduled tasks for the entire system
+	cat environment/scheduled-tasks/*.crontab | crontab -
+fi
---
+++ services/research-people.js
@@ -0,0 +1,135 @@
+#! /usr/bin/node
+
+// Constants
+let rootDir = __dirname + "/..";
+
+// Our custom imports
+let log = require( `${ rootDir }/lib/logger.js` );
+let dbms = require( `${ rootDir }/lib/dbms.js` );
+let telephone = require( `${ rootDir }/lib/telephone.js` );
+let people = require( `${ rootDir }/lib/people.js` );
+
+
+
+
+
+( async function main () {
+
+	let context = "Researching people.";
+
+	let databaseClient = await dbms.getClient();
+	let database = databaseClient.db( "cupid" );
+	let collection = database.collection( "people" );
+
+	/*
+	 * 1. Get all the un-"Researched" people
+	 */
+	let timestamp__CoupleHoursAgo = new Date( Date.now() - 2 * 60 * 60 * 1000 );
+	let peopleRecords = await collection.find( {
+		// "actions.research": { $in: [ null, false ] }
+			// has not been operated on
+		"actions.research": { $ne: true },
+			// does not have an error
+		"meta.error": { $ne: true }
+			// was added in the last two hours
+		"meta.createdOn": { $gte: timestamp__CoupleHoursAgo },
+	} ).sort( { "meta.createdOn": 1 } );
+
+	/*
+	 * 2. Run the people records through the research process
+	 */
+	while ( await peopleRecords.hasNext() ) {
+
+		let person = await peopleRecords.next();
+
+		/*
+		 * A. Verify the phone number
+		 */
+		let phoneNumberInformation;
+		try {
+			phoneNumberInformation = await telephone.isPhoneNumberValid( person.phoneNumber );
+		}
+		catch ( e ) {
+				// Log the error
+			await log.toUs( {
+				context: `Determining if the phone number of the person ( id ${ person._id.toString() } ) is legit`,
+				message: context + "\n```\n" + e.stack + "\n```"
+			} );
+				// Update the person record with the error flag
+			await collection.updateOne( { _id: person._id }, { $set: { "meta.error": true } } );
+			continue;
+		}
+		if ( phoneNumberInformation.success === false ) {
+				// Log the error
+			let error = phoneNumberInformation.error;
+			log.toUs( {
+				context: `Determining if the phone number of the person ( id ${ person._id.toString() } ) is legit`,
+				message: `${ context }\n[${ e.code }] ${ e.info }`
+			} );
+				// Update the person record with the error flag
+			await collection.updateOne( { _id: person._id }, { $set: { "meta.error": true } } );
+			continue;
+		}
+		if ( ! phoneNumberInformation.valid ) {
+			await collection.updateOne( { _id: person._id }, { $set: {
+				"meta.spam": true,
+				"meta.phoneNumberIsValid": false,
+				"actions.validatePhoneNumber": true,
+				"actions.research": true
+			} } );
+			continue;
+		}
+		await collection.updateOne( { _id: person._id }, { $set: {
+			"actions.validatePhoneNumber": true
+		} } );
+
+		/*
+		 * B. Search for information
+		 */
+		let phoneNumbers = [ person.phoneNumber ].concat( ( person.contact && person.contact.otherPhoneNumbers ) || [ ] );
+		let emailAddresses = [ person.emailAddress ].concat( ( person.contact && person.contact.otherEmailAddresses ) || [ ] );
+		try {
+			informationOnPerson = await people.getDataOnPerson( phoneNumbers, emailAddresses );
+		}
+		catch ( e ) {
+				// Log the error
+			await log.toUs( {
+				context: `Querying information on a person ( id ${ person._id.toString() } )`,
+				message: context + "\n```\n" + e.stack + "\n```"
+			} );
+				// Update the person record with the error flag
+			await collection.updateOne( { _id: person._id }, { $set: { "meta.error": true } } );
+			continue;
+		}
+		if ( Array.isArray( informationOnPerson ) )
+			await log.toUs( {
+				context: `Querying information on a person ( id ${ person._id.toString() } )`,
+				message: "More than one match found for the person."
+			} );
+		else {
+			// person = Object.assign( person, informationOnPerson );
+			await collection.updateOne( {
+				_id: person._id
+			}, { $set: informationOnPerson } );
+		}
+
+		await collection.updateOne( { _id: person._id }, { $set: {
+			"actions.gatherInformation": true,
+			"meta.fetchedInformationOn": new Date()
+		} } );
+
+		/*
+		 * C. Finally, ackowledge that the research has been complete
+		 */
+		await collection.updateOne( { _id: person._id }, { $set: {
+			"actions.research": true
+		} } );
+
+	}
+
+	/*
+	 * We're done.
+	 */
+	process.exit( 0 );
+
+}() );
