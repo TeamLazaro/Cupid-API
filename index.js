864553a8f3cb23ef4e6caa93177723febeea56a5
adi <52442230+adi-lazaro@users.noreply.github.com>
2019-07-31 10:26:38 +0530

    People : Verify : Remove the `interest` parameter from consideration

--- index.js
+++ index.js
@@ -263,13 +263,10 @@ router.post( "/people/verify", async function ( req, res ) {
 	res.header( "Access-Control-Allow-Credentials", "true" );
 
 	let client = req.body.client || req.query.client;
-	let interest = req.body.interest || req.query.interest;
 	let phoneNumbers = req.body.phoneNumbers || req.query.phoneNumbers;
 
 	if (
 		! client
-			||
-		! interest
 			||
 		! phoneNumbers
 	) {
@@ -292,7 +289,6 @@ router.post( "/people/verify", async function ( req, res ) {
 	let record = await collection.updateOne(
 		{
 			client,
-			interest,
 			phoneNumber: { $in: phoneNumbers }
 		},
 		{ $set: { "meta.identityVerified": true } }
