c367957c61d4455b364aada8eed861ad1b6254ae
adi <52442230+adi-lazaro@users.noreply.github.com>
2019-08-08 12:18:52 +0530

    Service : Report Allocations of Customers to Salespeople

--- endpoints/index.js
+++ endpoints/index.js
@@ -15,6 +15,7 @@ router = require( "./get-people.js" )( router, middleware );
 router = require( "./add-person.js" )( router, middleware );
 router = require( "./verify-person.js" )( router, middleware );
 router = require( "./research-customer-by-crm-id.js" )( router, middleware );
+router = require( "./v1/report-customer-allocation.js" )( router, middleware );
 
 
 
