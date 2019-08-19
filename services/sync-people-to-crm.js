f5ca6bc6198384fd071738a381419fc1766487ae
adi <52442230+adi-lazaro@users.noreply.github.com>
2019-08-19 11:51:22 +0530

    Service : Sync People to CRM : Add some error handling

--- services/sync-people-to-crm.js
+++ services/sync-people-to-crm.js
@@ -20,6 +20,33 @@ let crm = require( `${ rootDir }/lib/crm.js` );
 
 
 
+/*
+ *
+ * Handle un-caught/handled exceptions and errors
+ *
+ */
+async function shutdownGracefully ( e ) {
+	let context = "There was an uncaught error or unhandled rejection";
+	let message = e.toString();
+	if ( e.stack )
+		message += "\n```\n" + e.stack + "\n```";
+	await log.toUs( {
+		context: context,
+		message: message
+	} );
+	console.error( context + "\n" + message );
+	setTimeout( function () {
+		console.log( "Terminating process right now." );
+		process.exit( 1 );
+	}, 1000 );
+}
+process.on( "uncaughtException", shutdownGracefully );
+process.on( "unhandledRejection", shutdownGracefully );
+
+
+
+
+
 ( async function main () {
 
 	let context = "Routine syncing of people onto the CRM.";
