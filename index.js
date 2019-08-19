d30f67ce28703c5cc74e2538df1214a1a153224d
adi <52442230+adi-lazaro@users.noreply.github.com>
2019-08-19 11:37:35 +0530

    Error Handling : Gracefully shut down the process when errors are thrown

--- index.js
+++ index.js
@@ -43,3 +43,28 @@ process.on( "SIGINT", function ( signal, code ) {
 	log.toConsole( "All done." );
 
 } );
+
+// When unexpected and unhandled exceptions or errors are thrown
+async function shutdownGracefully ( e ) {
+
+	let context = "There was an uncaught error or unhandled rejection";
+	let message = e.toString();
+	if ( e.stack )
+		message += "\n```\n" + e.stack + "\n```";
+
+	if ( log && log.toUs )
+		await log.toUs( {
+			context: context,
+			message: message
+		} );
+
+	console.error( context + "\n" + message );
+
+	setTimeout( function () {
+		console.log( "Terminating process right now." );
+		process.exit( 1 );
+	}, 1000 );
+
+}
+process.on( "uncaughtException", shutdownGracefully );
+process.on( "unhandledRejection", shutdownGracefully );
