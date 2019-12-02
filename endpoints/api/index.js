a2431a87aa393c3f6a694950e5b32aead898d49c
adi <52442230+adi-lazaro@users.noreply.github.com>
2019-12-02 19:50:28 +0530

    Routes : API : Public : Install an API key verifying middleware

--- endpoints/api/index.js
+++ endpoints/api/index.js
@@ -9,6 +9,9 @@ function main ( router, middleware ) {
 	// Create a router
 	let apiRouter = require( "express" ).Router();
 
+
+	apiRouter.use( middleware.verifyAPIKey );
+
 	// Use the API router for all `/api/` routes
 	router.use( "/api/v1/", apiRouter );
 
