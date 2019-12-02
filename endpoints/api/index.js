9f2490657a02c8a80e9630fa9941149356e46de2
adi <52442230+adi-lazaro@users.noreply.github.com>
2019-12-02 19:49:07 +0530

    Routes : API : Public : Create a dedicated router

--- endpoints/api/index.js
+++ endpoints/api/index.js
@@ -6,6 +6,12 @@
  */
 function main ( router, middleware ) {
 
+	// Create a router
+	let apiRouter = require( "express" ).Router();
+
+	// Use the API router for all `/api/` routes
+	router.use( "/api/v1/", apiRouter );
+
 	return router;
 
 }
