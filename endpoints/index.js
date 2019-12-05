c45fc2c903727d754590340a1d757fe9f14de0e2
adi <52442230+adi-lazaro@users.noreply.github.com>
2019-12-05 11:31:30 +0530

    Routes : Research Customer by CRM Id : Remove this one

--- endpoints/index.js
+++ endpoints/index.js
@@ -23,7 +23,6 @@ router = require( "./v2/update-person.js" )( router, middleware );
 router = require( "./v2/hooks/post-call.js" )( router, middleware );
 router = require( "./v2/hooks/post-id-assignment.js" )( router, middleware );
 router = require( "./v2/hooks/person-on-website.js" )( router, middleware );
-router = require( "./research-customer-by-crm-id.js" )( router, middleware );
 router = require( "./v1/report-customer-allocation.js" )( router, middleware );
 router = require( "./v1/report-customer-concluded-meetings.js" )( router, middleware );
 
